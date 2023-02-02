30e0a94749c8221ec1314f771ad70a586d1fa5e3
Kurt Vermillion <kurtv0727@gmail.com>
2023-02-01 23:29:38 -0500

    rails lab

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,11 @@
 class Student < ApplicationRecord
 
+  # scope :by_name, ->(name) { where("first_name LIKE ? OR last_name LIKE ?", "%#{name}%", "%#{name}%") }
+
+  def self.by_name(name)
+    where("first_name LIKE ? OR last_name LIKE ?", "%#{name}%", "%#{name}%")
+  end
+  
   def to_s
     "#{first_name} #{last_name}"
   end
